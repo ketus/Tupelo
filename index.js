69b7a1e824edc3323a98ff8f788c3a4d2f415763
Ketus-home <maciej.oleksa@gmail.com>
2016-05-18 22:53:25 +0200

    bug fix

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var colors = require('colors');
 
 var tupelo = exports;
-
+tupelo.mode = '';
 tupelo.debugLevel = 'warn';
-tupelo.logToFile = true;
+//tupelo.logToFile = true;
 
-
-function tupelo(level, message) {
+tupelo.log = function(level, message) {
     var levels = ['error', 'warn', 'info'];
-    if (levels.indexOf(level) >= levels.indexOf(logger.debugLevel)) {
-        if (typeof message !== 'string') {
+    if (debugLevels.indexOf(level) >= levels.indexOf(tupelo.debugLevel)) {
+        if (message && typeof message !== 'string') {
             message = JSON.stringify(message);
         };
-        console.log(level + ': ' + message);
+        console.log((level + ': ' + message).green);
     }
 }
